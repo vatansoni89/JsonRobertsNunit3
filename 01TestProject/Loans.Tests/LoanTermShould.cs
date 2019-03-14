b3327a78dedae00c6525d8d1c73b0c7cbfc62ebd
Vatan Soni <vatansoni89@gmail.com>
2019-03-14 17:56:44 +0530

    Completed till 5

---
+++ 01TestProject/Loans.Tests/LoanTermShould.cs
@@ -0,0 +1,91 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loans.Tests
+{
+    [TestFixture] //This class is atest class and contains Test methods.
+    public class LoanTermShould
+    {  
+        [Test] //make a method as test.
+        public void ReturnTermInMonths()
+        {
+            var sut = new LoanTerm(1); // sut = system under test
+
+            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+        }
+
+        [Test] //make a method as test.
+        public void ReturnTermInYear()
+        {
+            var sut = new LoanTerm(1); // sut = system under test
+
+            Assert.That(sut.Years, Is.EqualTo(1));
+        }
+        [Test]
+        public void RespectValueEqality()
+        {
+            var a = 1;
+            var b = 1;
+
+            Assert.That(a, Is.EqualTo(b));
+        }
+
+        [Test]
+        public void RespectValueEqality1() // Its passing bcz of override Equals in ValueObject
+        {
+            var a = new LoanTerm(1);
+            var b = new LoanTerm(1);
+
+            Assert.That(a, Is.EqualTo(b));
+        }
+
+        [Test] // Test if object are at same memory location.
+        public void ReferenceEqualityExample()
+        {
+            var a = new LoanTerm(1);
+            var b = a;
+            var c = new LoanTerm(1);
+
+            Assert.That(a, Is.SameAs(b));
+            //Assert.That(a, Is.SameAs(c));  Fails
+            Assert.That(a, Is.Not.SameAs(c));
+        }
+
+        [Test]
+        public void Double()
+        {
+            double a = 1.0 / 3;
+
+            var tt = Is.EqualTo(0.33).Within(0.004);
+            Assert.That(a, Is.EqualTo(0.33).Within(0.004));
+
+            Assert.That(a, Is.EqualTo(0.33).Within(10).Percent);
+        }
+
+        //Exception test
+        [Test]
+        public void NotAllowedZeroYears()
+        {
+            Assert.That(() => new LoanTerm(0), Throws.TypeOf<ArgumentOutOfRangeException>());
+
+            //Checks the meaage thrown as expected or not.
+            Assert.That(() => new LoanTerm(0), Throws.TypeOf<ArgumentOutOfRangeException>()
+                .With.Property("Message").EqualTo("Please specify a value greater than 0.\r\nParameter name: years"));
+
+            Assert.That(() => new LoanTerm(0), Throws.TypeOf<ArgumentOutOfRangeException>()
+                .With.Message.EqualTo("Please specify a value greater than 0.\r\nParameter name: years"));
+
+            //Jst care for ParamName
+            Assert.That(() => new LoanTerm(0), Throws.TypeOf<ArgumentOutOfRangeException>()
+                .With.Property("ParamName").EqualTo("years"));
+
+            //Jst care for ParamName using Lambda
+            Assert.That(() => new LoanTerm(0), Throws.TypeOf<ArgumentOutOfRangeException>()
+                .With
+                .Matches<ArgumentOutOfRangeException>(ex => ex.ParamName == "years"));
+        }
+    }
+}
---
+++ 01TestProject/Loans.Tests/ProductComparerShould.cs
@@ -0,0 +1,82 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loans.Tests
+{
+    [TestFixture]
+    public class ProductComparerShould
+    {
+        private List<LoanProduct> products;
+        private ProductComparer sut;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup() //Execute only one time before the first test.
+        {
+            products = new List<LoanProduct>
+            {
+                new LoanProduct(1,"a",1),
+                new LoanProduct(2,"b",2),
+                new LoanProduct(3,"c",3)
+            };
+        }
+
+        [SetUp]
+        public void SetUp() //run before each test
+        {
+             sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+        }
+
+        [TearDown] //run after each test
+        public void TearDown()
+        {
+
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()// run after last test executed.
+        {
+
+        }
+
+        [Test]
+        [Category("Product Comparison")]
+        public void ReturnCorrectnumberOfComparision()
+        {   
+
+            List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            Assert.That(comparisons, Has.Exactly(3).Items);
+
+            Assert.That(comparisons, Is.Unique);
+
+            var expectedProduct = new MonthlyRepaymentComparison("a", 1, 643.28m);
+
+            //Test if expected product is there.
+            Assert.That(comparisons, Does.Contain(expectedProduct));
+        }
+
+        [Test]
+        public void ReturnComparisionForFirstProduct_WithPartialKnownExpectedValues()
+        {
+
+            List<MonthlyRepaymentComparison> comparisons = sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            //Check Property
+            Assert.That(comparisons, Has.Exactly(1)
+                .Property("ProductName").EqualTo("a")
+                .And
+                .Property("MonthlyRepayment").GreaterThan(0));
+
+            //Check property by lambda expression
+            Assert.That(comparisons, Has.Exactly(1)
+                .Matches<MonthlyRepaymentComparison>(
+                item => item.ProductName == "a" &&
+                item.InterestRate == 1 &&
+                item.MonthlyRepayment > 0));
+        }
+    }
+}
+ 
